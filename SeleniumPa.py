2b247f01ca4220fb3ef642a9136dafcc51cb1081
Louis <qingyanjiu@126.com>
2017-12-03 21:47:28 +0800

    enhanement

--- SeleniumPa.py
+++ SeleniumPa.py
@@ -4,9 +4,13 @@ import urllib
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common import exceptions
 from time import sleep
-import json
+import pytesseract
+from PIL import Image
 import codecs
+
+searchDate = '2010-09-29'
 
 def write_txt(text):
     f = codecs.open('C:\\ids\\刑事案件.txt', 'a', 'utf8')
@@ -24,26 +28,69 @@ def openUrl(params):
     getData(driver)
 
 def getData(driver):
-    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME,'dataItem')))
-    ids = driver.find_elements_by_class_name("DocIds")
-    if(len(ids) > 0):
-        for id in ids:
-            write_txt(id.get_property('value') + ';')
-        driver.execute_script("$('.next').trigger('click')")
-        sleep(5)
-        if(driver.find_elements_by_class_name('next')[0].value_of_css_property('color') != 'rgba(153, 153, 153, 1)'):
-            getData(driver)
+    try:
+        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME,'dataItem')))
+        ids = driver.find_elements_by_class_name("DocIds")
+        if(len(ids) > 0):
+            for id in ids:
+                write_txt(id.get_property('value') + ';\n')
+            driver.execute_script("$('.next').trigger('click')")
+            sleep(5)
+            if(driver.find_elements_by_class_name('next')[0].value_of_css_property('color') != 'rgba(153, 153, 153, 1)'):
+                getData(driver)
+            else:
+                driver.close()
+                updateSearchDate(searchDate)
+                openUrl(getParams(searchDate))
+    except exceptions.TimeoutException:
+        if(driver.current_url == 'http://wenshu.court.gov.cn/Html_Pages/VisitRemind.html'):
+            print(Image.PILLOW_VERSION)
+            url = "http://wenshu.court.gov.cn/User/ValidateCode"
+            f = open('C:\\ids\\vcode.jpeg', 'wb')
+            req = urllib.request.urlopen(url)
+            buf = req.read()
+            f.write(buf)
+            sleep(5)
+            image = Image.open(r'C:\ids\vcode.jpeg')
+            vcode = pytesseract.image_to_string(image)
+            print(vcode)
+            driver.execute_script("$('#txtValidateCode').val("+vcode+")")
+            driver.execute_script("$('.btn_validatecode').trigger('click')")
         else:
             driver.close()
-            date = "2017-12"
-            openUrl(getParams(date))
+            updateSearchDate(searchDate)
+            openUrl(getParams(searchDate))
+
+
 
 def getParams(date):
-    params = '裁判日期:'+date+'-01 TO '+date+'-31'
+    params = '裁判日期:'+date+' TO '+date
+    print(params)
     return params
 
+def updateSearchDate(date):
+    year = date.split('-')[0]
+    month = date.split('-')[1]
+    day = date.split('-')[2]
+    if(day != '31'):
+        day = int(day) + 1
+        if(day < 10):
+            day = '0' + str(day)
+    else:
+        day = '01'
+        if(month != '12'):
+            month = int(month) + 1
+            if(month < 10):
+                month = '0' + str(month)
+        else:
+            month = '01'
+            year = int(year) + 1
+
+    global searchDate
+    searchDate = str(year) + '-' + str(month) + '-' + str(day)
+
 def main():
-    params = getParams("2017-12")
+    params = getParams(searchDate)
     openUrl(params)
 
 if __name__ == '__main__':
