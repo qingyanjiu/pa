54ad5ffb686e51150beceec246a9394ce71a197b
Louis <qingyanjiu@126.com>
2017-12-03 15:13:41 +0800

    test

--- SeleniumPa.py
+++ SeleniumPa.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 from selenium import webdriver
 import urllib
 from selenium.webdriver.support.wait import WebDriverWait
@@ -8,27 +9,34 @@ import json
 import codecs
 
 def write_txt(text):
-    f = codecs.open('C:\\ids\\刑事案件.txt', 'a', 'utf8')
+    f = codecs.open('C:\\刑事案件.txt', 'a', 'utf8')
     f.write(str(text))
     f.close()
 
-def openUrl():
+def openUrl(params):
     driver = webdriver.Chrome()
-    driver.get('http://wenshu.court.gov.cn/list/list?sorttype=1&conditions=searchWord+1+AJLX++案件类型:刑事案件')
+    driver.get('http://wenshu.court.gov.cn/List/List?sorttype=1&conditions=searchWord+1+AJLX++%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E5%88%91%E4%BA%8B%E6%A1%88%E4%BB%B6')
+    driver.execute_script("$('#gover_search_key').val('"+params+"')")
+    driver.execute_script("$('#btnSearch').trigger('click')")
     getData(driver)
 
 def getData(driver):
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME,'dataItem')))
     ids = driver.find_elements_by_class_name("DocIds")
-    if(len(ids) == 5):
+    if(len(ids) > 0):
         for id in ids:
             write_txt(id.get_property('value') + ';')
         sleep(10)
         driver.execute_script("$('.next').trigger('click')")
         getData(driver)
 
+def getParams():
+    params = '裁判日期:2017-12-01 TO 2017-12-31'
+    return params
+
 def main():
-    openUrl()
+    params = getParams()
+    openUrl(params)
 
 if __name__ == '__main__':
     main()
