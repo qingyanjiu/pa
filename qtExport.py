3b88d22b3944b5328f0880c9e5b0e1757a72974b
Louis <qingyanjiu@126.com>
2017-12-04 22:06:34 +0800

    added validation test file

--- qtExport.py
+++ qtExport.py
@@ -1,12 +1,14 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
-import urllib
-from time import sleep
-import json
+import codecs
 
+def write_txt(text):
+    f = codecs.open('C:\\ids\\刑事案件.txt', 'a', 'utf8')
+    f.write(str(text))
+    f.close()
 
-def main():
+def openUrl():
     app = QApplication([])
     webview= QWebView()
     loop = QEventLoop()
@@ -14,8 +16,22 @@ def main():
     webview.load(QUrl('http://wenshu.court.gov.cn/list/list?sorttype=1&conditions=searchWord+1+AJLX++案件类型:刑事案件'))
     loop.exec_()
     webview.show()
+    getData(app,webview)
+
+def getData(app,webview):
     frame = webview.page().mainFrame()
-    frame.findAllElements('#DocIds')
+    ids = frame.findAllElements('.DocIds')
+    print(len(ids))
+    if(len(ids) == 5):
+        for id in ids:
+            write_txt(id.get_property('value') + ';')
+        frame.findFirstElement(".next").evaluateJavaScript('this.click()')
+        getData(app,webview)
+        app.exec_()
+
+
+def main():
+    openUrl()
 
 if __name__ == '__main__':
     main()
